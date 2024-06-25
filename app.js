f65cac671af075ff5a8f1c326bff378c8eccbd57
FedeBurgos <federico.burgos@mi.unc.edu.ar>
2024-06-25 17:06:52 -0300

    Add documetation

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+// Declaracion de variables
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
 
 
-
+/*
+    @brief Funcion que coloca el texto en el elemento HTML correspondiente.
+    @param elemento Elemento HTML.
+        texto Texto que se quiere mostrar.
+*/
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
 
+/*
+    @brief Funcion que verifica si se acierta el numero o en caso contrario da pistas.
+*/
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     
@@ -30,10 +38,16 @@ function verificarIntento() {
     return;
 }
 
+/*
+    @brief Funcion que limpia la caja donde el usuario tipea el numero.
+*/
 function limpiarCaja() {
     document.querySelector('#valorUsuario').value = '';
 }
 
+/*
+    @brief Funcion que genera el numero secreto y comprueba que no se repita.
+*/
 function generarNumeroSecreto() {
     let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
 
@@ -53,6 +67,9 @@ function generarNumeroSecreto() {
     }
 }
 
+/*
+    @brief Funcion que setea las condiciones iniciales del juego.
+*/
 function condicionesIniciales() {
     asignarTextoElemento('h1','Juego del número secreto!');
     asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
@@ -61,6 +78,10 @@ function condicionesIniciales() {
     console.log(numeroSecreto);
 }
 
+/*
+    @brief Funcion que reinicia el juego limpiando la caja, seteando las condiciones inicales
+    y deshabilitando el boton de Nuevo juego.
+*/
 function reiniciarJuego() {
     //limpiar caja
     limpiarCaja();
